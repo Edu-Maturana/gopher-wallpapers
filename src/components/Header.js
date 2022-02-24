8efc843bd60b81028690eab8b8cfba3989bae870
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-02-24 19:12:28 -0300

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,9 @@ import "./styles.css"
 
 export default function Header() {
   return (
-    <div className='header'>Header</div>
+    <div className='header'>
+        <img className='logo' src="https://res.cloudinary.com/edu-golang-docker-8080/image/upload/v1645738948/LOGO_mskagr.png" alt='logo' />
+        <h1>Gopher Wallpapers</h1>
+    </div>
   )
 }
