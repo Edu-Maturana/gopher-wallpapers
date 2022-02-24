a3b568959a2f055bed53646adaab408739fddf37
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-02-24 20:12:34 -0300

    disable https warning

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,4 +1,4 @@
-const apiUrl = "http://ec2-54-234-224-87.compute-1.amazonaws.com:8080/wallpapers";
+const apiUrl = "ec2-54-234-224-87.compute-1.amazonaws.com:8080/wallpapers";
 
 export const getWallpapers = async () => {
   const response = await fetch(apiUrl);
