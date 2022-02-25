80eacb353cd249cd99a8d3d13eda23404458fa36
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-02-24 21:31:18 -0300

    add json

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,7 +1,32 @@
-const apiUrl = "http://ec2-54-234-224-87.compute-1.amazonaws.com:8080/wallpapers";
-
-export const getWallpapers = async () => {
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  return data;
-};
+export const wallpapers = [
+  {
+    id: "c8bfghis1s428ciqi3hg",
+    title: "Astronaut Gopher",
+    img: "https://res.cloudinary.com/edu-golang-docker-8080/image/upload/v1645672236/gopher_xrjuep.png",
+    date: "2022-02-24 00:15:18",
+  },
+  {
+    id: "c8bvts1r0h1639been70",
+    title: "Gopher & Logo",
+    img: "https://res.cloudinary.com/edu-golang-docker-8080/image/upload/v1645730116/gopher_go_pr6j4z.png",
+    date: "2022-02-24 21:56:00",
+  },
+  {
+    id: "c8bvu4pr0h1639been7g",
+    title: "Gopher Synthwave",
+    img: "https://res.cloudinary.com/edu-golang-docker-8080/image/upload/v1645735976/synth_vkt1l4.png",
+    date: "2022-02-24 21:56:35",
+  },
+  {
+    id: "c8bvu9hr0h1639been80",
+    title: "Hello world!",
+    img: "https://res.cloudinary.com/edu-golang-docker-8080/image/upload/v1645736466/hello_world_ugkptp.png",
+    date: "2022-02-24 21:56:54",
+  },
+  {
+    id: "c8bvudpr0h1639been8g",
+    title: "Captain Gopher",
+    img: "https://res.cloudinary.com/edu-golang-docker-8080/image/upload/v1645738203/captain_mnec7t.png",
+    date: "2022-02-24 21:57:11",
+  },
+];
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,31 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./styles.css";
-import { getWallpapers } from "../api/fetch";
+import wallpapers from "..fetch/api/fetch";
 
 export default function Content() {
-  const [wallpapers, setWallpapers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    getWallpapers().then((data) => {
-      setWallpapers(data);
-      setLoading(false);
-    });
-  }, []);
-
-  return <div className="content">
-    {loading ? <div>Loading...</div> : <div>
-        {wallpapers.map((wallpaper) => {
-            return <div className="wallpaper" key={wallpaper.id}>
-                <img src={wallpaper.img} alt={wallpaper.title} />
-                <div className="data">
-                <h3>{wallpaper.title}</h3>
-                <a href={wallpaper.img} target="_blank" rel="noopener noreferrer">
-                    Download
-                </a>
-                </div>
+  return (
+    <div className="content">
+      {wallpapers.map((wallpaper) => {
+        return (
+          <div className="wallpaper" key={wallpaper.id}>
+            <img src={wallpaper.img} alt={wallpaper.title} />
+            <div className="data">
+              <h3>{wallpaper.title}</h3>
+              <a href={wallpaper.img} target="_blank" rel="noopener noreferrer">
+                Download
+              </a>
             </div>
-        })}
-    </div>}
-  </div>;
+          </div>
+        );
+      })}
+    </div>
+  );
 }
